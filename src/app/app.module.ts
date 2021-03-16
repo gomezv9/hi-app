b81bc7b181737e2b9794111218112a53544d17cd
Jaime Gomez <gomezv9@hotmail.com>
2021-03-15 19:26:43 -0500

    fisrt commit, capacities component with only wrapper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CapacitiesComponent } from './capacities/capacities.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapacitiesComponent
   ],
   imports: [
     BrowserModule,
